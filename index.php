f2e0061205a5e9b31f458409d707b92fc4a53752
rbaldini69 <rbaldini69@hotmail.com>
2016-04-05 00:28:28 -0300

    Update and rename index.html to index.php
    cambio extension del archivo y agrego tags php a valor ("<?php ejemplo ;?>")

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 	
 
-</head>
+<h1 style="text-align: center; font-size: 15;text-decoration-color: #B40431"> CALCULO DE MATERIALES FERRETERIA FERRETE </h1>
 <body style="background-color: lightblue">
 <div style="border-style: solid;border-color: orange;margin-left: 28%; margin-right: 40%">
 
 <form action="calculo.php" method="GET">
 <br>
 	<div style="background-color: #FBEFEF">
-	<br> <input style="font-size: 16px; height: 40px; width: 200px; margin-right: 15%; margin-left: 18%; border-radius:5px" value="<?php echo $largo ?>" type="double" name="largo" placeholder="<?php echo $largo?>" ></br>
-	<br> <input style="font-size: 16px;height: 40px;width: 200px;margin-left: 18%; border-radius:5px"type="double" value="<?php echo $ancho ?>" name="ancho" placeholder="<?php echo $ancho?>" ></br>
+	<br> <input style="font-size: 16px; height: 40px; width: 200px; margin-right: 15%; margin-left: 18%; border-radius:5px" value="<?php echo $largo; ?>" type="double" name="largo"></br>
+	<br> <input style="font-size: 16px;height: 40px;width: 200px;margin-left: 18%; border-radius:5px"type="double" value="<?php echo $ancho; ?>" name="ancho"></br>
 
-	<br> <input style="font-size: 16px;height: 40px;width: 200px;margin-left: 18%; border-radius:5px" type="double" value="<?php echo $radio ?>" name="radio"  placeholder="<?php echo $radio ?>" ></br>
+	<br> <input style="font-size: 16px;height: 40px;width: 200px;margin-left: 18%; border-radius:5px" type="double" value="<?php echo $radio; ?>" name="radio"></br>
 	<p><br></p>
 	
 	
@@ -39,6 +40,12 @@
 </div>
 <br>
 </div>
+
+</form>
+
+</body>
+
+</html>
 
 </form>
 
